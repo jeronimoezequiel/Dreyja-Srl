6a7d0cac76935a9ee84f92b06d601440a94d0c7b
jeronimo_nuñez <jeronimonunez96@gmail.com>
2023-10-21 11:36:42 -0300

    responsive creado

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('ul.tabs li a:first').addClass('active');
+
+    $('ul.tabs li a').click(function(){
+        $('ul.tabs li a').removeClass('active');
+        $(this).addClass('active');
+    });
+});
