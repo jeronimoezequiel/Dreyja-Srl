ee7e42d97bdcf55e4b471f3800bff9cbdadf8097
jeronimo_nuñez <jeronimonunez96@gmail.com>
2023-11-01 19:47:56 -0300

    Index finalizado, arreglar detalles

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-$(document).ready(function(){
-    $('ul.tabs li a:first').addClass('active');
+// $(document).ready(function(){
+//     $('ul.tabs li a:first').addClass('active');
 
-    $('ul.tabs li a').click(function(){
-        $('ul.tabs li a').removeClass('active');
-        $(this).addClass('active');
-    });
-});
+//     $('ul.tabs li a').click(function(){
+//         $('ul.tabs li a').removeClass('active');
+//         $(this).addClass('active');
+//     });
+// });
